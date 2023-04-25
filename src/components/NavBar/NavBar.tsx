3545c98d9b44626cd62713b5f77f9137715581db
Vlad Vasian <vlad.vasian96@gmail.com>
2023-04-25 19:11:36 +0300

    adding mobile navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import "./Navbar.style.css"
 import { Logo } from "../../assets/svg/Logo";
-import { Popover } from "@mui/material";
+import { Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Popover } from "@mui/material";
 import { ArrowDown } from "../../assets/svg/ArrowDown";
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import RouterIcon from '@mui/icons-material/Router';
+import HelpIcon from '@mui/icons-material/Help';
 import { useNavigate } from "react-router-dom";
 import Language from "../Language/Language";
 import { useTranslation } from "react-i18next";
+import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
 
     const [popoverOpened, setPopoverOpened] = useState(false);
     const [anchorProd, setAnchorProd] = useState<HTMLElement | null>(null)
+    const [openDrawer, setOpenDrawer] = useState(false)
 
     const navigate = useNavigate();
     const { t } = useTranslation();
@@ -22,85 +28,147 @@ const NavBar = () => {
 
 
     return (
-        <div className="navbar-container">
-            <div
-                className="logo"
-                onClick={() => navigate("/")}
-            >
-                <Logo />
-                Dacona
-            </div>
-            <div className="nav-content">
-                <ul className="nav-list">
-                    <li>
-                        <div
-                            role="button"
-                            onClick={handleClickProducts}
-                        >
-                            {t("products")}
-                            <ArrowDown />
-                        </div>
-                        <Popover
-                            open={popoverOpened}
-                            anchorEl={anchorProd}
-                            onClose={handleClickProducts}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            sx={{
-                                ".MuiPaper-rounded": {
-                                    backgroundColor: "#292929",
-                                    marginTop: "5px"
-                                }
-                            }}
-                        >
-                            <div className="dropdown-container">
-                                <ul>
-                                    <li
-                                        className="dropdown-element"
-                                        onClick={(e) => { navigate("/teltonika"); handleClickProducts(e) }}
-                                    >
-                                        <div>
-                                            Teltonika
-                                        </div>
-                                    </li>
-                                    <li className="dropdown-element">
-                                        <div>
-                                            Hikvision
-                                        </div>
-                                    </li>
-                                </ul>
+        <>
+            <div className="navbar-container">
+                <div
+                    className="logo"
+                    onClick={() => navigate("/")}
+                >
+                    <Logo />
+                    Dacona
+                </div>
+                <div className="nav-content">
+                    <ul className="nav-list">
+                        <li>
+                            <div
+                                role="button"
+                                onClick={handleClickProducts}
+                            >
+                                {t("products")}
+                                <ArrowDown />
                             </div>
-                        </Popover>
-                    </li>
-                    <li>
-                        <div
-                            role="button"
-                        >
-                            {t("features")}
-                        </div>
-                    </li>
-                    <li>
-                        <div
-                            role="button"
-                        >
-                            {t("pricing")}
-                        </div>
-                    </li>
-                    <li>
-                        <div
-                            role="button"
-                        >
-                            {t("support")}
-                        </div>
-                    </li>
-                </ul>
+                            <Popover
+                                open={popoverOpened}
+                                anchorEl={anchorProd}
+                                onClose={handleClickProducts}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                sx={{
+                                    ".MuiPaper-rounded": {
+                                        backgroundColor: "#292929",
+                                        marginTop: "5px"
+                                    }
+                                }}
+                            >
+                                <div className="dropdown-container">
+                                    <ul>
+                                        <li
+                                            className="dropdown-element"
+                                            onClick={(e) => { navigate("/teltonika"); handleClickProducts(e) }}
+                                        >
+                                            <div>
+                                                Teltonika
+                                            </div>
+                                        </li>
+                                        <li className="dropdown-element">
+                                            <div>
+                                                Hikvision
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Popover>
+                        </li>
+                        <li>
+                            <div
+                                role="button"
+                            >
+                                {t("features")}
+                            </div>
+                        </li>
+                        <li>
+                            <div
+                                role="button"
+                            >
+                                {t("pricing")}
+                            </div>
+                        </li>
+                        <li>
+                            <div
+                                role="button"
+                            >
+                                {t("support")}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="language-change">
+                    <Language />
+                </div>
             </div>
-            <div className="language-change">
-                <Language />
+            <div className="nav-mobile">
+                <IconButton onClick={() => setOpenDrawer(true)} sx={{ position: "absolute", right: "20px", top: "10px" }}>
+                    <MenuIcon sx={{ color: "#ffffff" }} fontSize="large" />
+                </IconButton>
+                <Drawer
+                    anchor={"left"}
+                    open={openDrawer}
+                    onClose={() => setOpenDrawer(false)}
+                >
+                    <Box
+                        sx={{ width: 250, backgroundColor: "black", height: "100%", color: "#ffffff" }}
+                    >
+                        <List>
+                            <ListItem key={'Products'} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <Inventory2Icon sx={{ color: "#ffffff" }} />
+                                    </ListItemIcon>
+                                    <ListItemText primary={'Products'} />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem key={'Features'} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <RouterIcon sx={{ color: "#ffffff" }} />
+                                    </ListItemIcon>
+                                    <ListItemText primary={'Features'} />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem key={'Pricing'} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <LocalOfferIcon sx={{ color: "#ffffff" }} />
+                                    </ListItemIcon>
+                                    <ListItemText primary={'Pricing'} />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem key={'Support'} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <HelpIcon sx={{ color: "#ffffff" }} />
+                                    </ListItemIcon>
+                                    <ListItemText primary={'Support'} />
+                                </ListItemButton>
+                            </ListItem>
+                        </List>
+                        <Divider sx={{ backgroundColor: "#FFFFFF" }} />
+                        <List>
+                            <ListItem key={"language"} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        TT
+                                    </ListItemIcon>
+                                    <Language />
+                                </ListItemButton>
+                            </ListItem>
+                        </List>
+                    </Box>
+                </Drawer>
             </div>
-        </div>
+        </>
     )
 }
 
