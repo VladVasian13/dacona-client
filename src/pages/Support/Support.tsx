dccc1474c3ed922ed7e6a062907eb734dfda59b6
Vlad Vasian <vlad.vasian96@gmail.com>
2023-07-27 22:33:39 +0300

    finishing support

--- src/pages/Support/Support.tsx
+++ src/pages/Support/Support.tsx
@@ -1,7 +1,11 @@
 import React, { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
 import { Alert, Box, Button, CardActions, CircularProgress, Grid, InputLabel, Paper, Snackbar, TextField, Typography } from "@mui/material";
-
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import L from "leaflet"
+import MarkerPic from "../../assets/images/marker.png"
+import 'leaflet/dist/leaflet.css';
+import { AlternateEmail, LocalPhone, LocationOn } from "@mui/icons-material";
 
 const Support = () => {
     const form: any = useRef();
@@ -23,19 +27,51 @@ const Support = () => {
             })
     };
 
+    const customMarker = new L.Icon({
+        iconUrl: MarkerPic,
+        iconSize: new L.Point(30, 45),
+    });
+
     return (
         <>
-            <Grid container>
-                <Grid item xs={12} sx={{ p: 2 }}>
+            <Box sx={{ color: "white", pl: 5 }}>
+                <Typography sx={{ fontSize: "28px" }}>
+                    Contact us
+                </Typography>
+                <Box sx={{ display: "flex", fontSize: "18px", justifyContent: "space-around" }}>
+                    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "10px" }}>
+                        <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
+                            <LocationOn />
+                            Address
+                        </Box>
+                        Romania, Brasov, Str. Vasile Carlova, Nr. 16
+                    </Box>
+                    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "10px" }}>
+                        <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
+                            <LocalPhone />
+                            Phone
+                        </Box>
+                        0268 420 832
+                    </Box>
+                    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "10px" }}>
+                        <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
+                            <AlternateEmail />
+                            Email
+                        </Box>
+                        office@dacona.ro
+                    </Box>
+                </Box>
+            </Box>
+            <Grid container sx={{ p: 5 }}>
+                <Grid item xs={12} md={7} sx={{ m: 0, p: "0px !important" }}>
                     <form ref={form} onSubmit={sendEmail}>
-
-                        <Paper elevation={3} sx={{ marginRight: "15%", marginLeft: "15%" }}>
+                        <Paper sx={{ padding: 2 }}>
                             <Box sx={{ padding: 2 }}>
                                 <Typography variant="h6" gutterBottom sx={{ paddingBottom: 5 }}>
                                     Request an offer
                                 </Typography>
-                                <Grid container spacing={3}>
-                                    <Grid item xs={12} sm={2}>
+                                <Grid container spacing={3} sx={{ padding: "0px !important" }}>
+                                    <Grid item xs={12} sm={2} sx={{ padding: "12px 0px 0px 0px !important" }}>
                                         <InputLabel
                                             sx={{
                                                 display: "flex",
@@ -46,7 +82,7 @@ const Support = () => {
                                             Name
                                         </InputLabel>
                                     </Grid>
-                                    <Grid item xs={12} sm={10}>
+                                    <Grid item xs={12} sm={10} sx={{ padding: "12px 0px 0px 0px !important" }}>
                                         <TextField
                                             required
                                             id="from_name"
@@ -58,7 +94,7 @@ const Support = () => {
                                             variant="outlined"
                                         />
                                     </Grid>
-                                    <Grid item xs={12} sm={2}>
+                                    <Grid item xs={12} sm={2} sx={{ padding: "12px 0px 0px 0px !important" }}>
                                         <InputLabel
                                             sx={{
                                                 display: "flex",
@@ -69,7 +105,7 @@ const Support = () => {
                                             Email
                                         </InputLabel>
                                     </Grid>
-                                    <Grid item xs={12} sm={10}>
+                                    <Grid item xs={12} sm={10} sx={{ padding: "12px 0px 0px 0px !important" }}>
                                         <TextField
                                             required
                                             type="email"
@@ -81,7 +117,7 @@ const Support = () => {
                                             variant="outlined"
                                         />
                                     </Grid>
-                                    <Grid item xs={12} sm={2}>
+                                    <Grid item xs={12} sm={2} sx={{ padding: "12px 0px 0px 0px !important" }}>
                                         <InputLabel
                                             sx={{
                                                 display: "flex",
@@ -92,19 +128,19 @@ const Support = () => {
                                             Phone Number
                                         </InputLabel>
                                     </Grid>
-                                    <Grid item xs={12} sm={10}>
+                                    <Grid item xs={12} sm={10} sx={{ padding: "12px 0px 0px 0px !important" }}>
                                         <TextField
                                             required
                                             id="author"
                                             name="reply_to"
-                                            label="PhoneNumber"
+                                            label="Phone Number"
                                             fullWidth
                                             size="medium"
                                             autoComplete="off"
                                             variant="outlined"
                                         />
                                     </Grid>
-                                    <Grid item xs={12} sm={2}>
+                                    <Grid item xs={12} sm={2} sx={{ padding: "12px 0px 0px 0px !important" }}>
                                         <InputLabel
                                             sx={{
                                                 display: "flex",
@@ -115,7 +151,7 @@ const Support = () => {
                                             Message
                                         </InputLabel>
                                     </Grid>
-                                    <Grid item xs={12} sm={10}>
+                                    <Grid item xs={12} sm={10} sx={{ padding: "12px 0px 0px 0px !important" }}>
                                         <TextField
                                             required
                                             id="outlined-multiline-static"
@@ -146,6 +182,16 @@ const Support = () => {
                             </Box>
                         </Paper>
                     </form>
+                </Grid>
+                <Grid item xs={12} md={5} sx={{ padding: "0px !important" }}>
+                    <MapContainer style={{ height: "576px" }} center={[45.6613094, 25.6059513]} zoom={13}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
+                        <Marker position={{ lat: 45.6613094, lng: 25.6059513 }} icon={customMarker}>
+                            <Popup>
+                                Dacona Brasov, Str. Vasile Carlova Nr. 16
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
                 </Grid>
             </Grid>
             <Snackbar
